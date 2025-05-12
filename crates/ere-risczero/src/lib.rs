ff2e1f808d73a365b8a9af781601d3011fa5d8b6
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-05-12 14:01:59 +0100

    fill in compile trait

--- crates/ere-risczero/src/lib.rs
+++ crates/ere-risczero/src/lib.rs
@@ -1,20 +1,20 @@
-use thiserror::Error;
+use compile::compile_risczero_program;
 use zkvm_interface::Compiler;
 
 mod compile;
 
+mod error;
+use error::RiscZeroError;
+
 #[allow(non_camel_case_types)]
 pub struct RV32_IM_RISCZERO_ZKVM_ELF;
-
-#[derive(Debug, Error)]
-pub enum RiscZeroError {}
 
 impl Compiler for RV32_IM_RISCZERO_ZKVM_ELF {
     type Error = RiscZeroError;
 
     type Program = Vec<u8>;
 
-    fn compile(_path_to_program: &std::path::Path) -> Result<Self::Program, Self::Error> {
-        todo!()
+    fn compile(path_to_program: &std::path::Path) -> Result<Self::Program, Self::Error> {
+        compile_risczero_program(path_to_program).map_err(RiscZeroError::from)
     }
 }
