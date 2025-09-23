1ab83ad3c797cabe9ebf49c1809d8c5cf66a2236
Ignacio Hagopian <jsign.uy@gmail.com>
2025-09-23 11:22:15 -0300

    zisk: add memlock unlimited (#145)
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- crates/ere-dockerized/src/lib.rs
+++ crates/ere-dockerized/src/lib.rs
@@ -420,6 +420,7 @@ impl zkVM for EreDockerizedzkVM {
             // requires at least 8G shared memory, here we set 16G for safety.
             ErezkVM::Zisk => cmd
                 .option("shm-size", "16G")
+                .option("ulimit", "memlock=-1:-1")
                 .inherit_env("ZISK_PREALLOCATE")
                 .inherit_env("ZISK_UNLOCK_MAPPED_MEMORY")
                 .inherit_env("ZISK_MINIMAL_MEMORY")
